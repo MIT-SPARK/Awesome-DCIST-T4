fb62606be9f1749eba587c059631008b0af7ea7e
Aaron Ray <aaronray@mit.edu>
2025-03-02 20:49:49 -0500

    Fix formatting

--- rsm_dcist/setup.py
+++ rsm_dcist/setup.py
@@ -1,26 +1,24 @@
 from setuptools import find_packages, setup
 
-package_name = 'rsm_dcist'
+package_name = "rsm_dcist"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     package_dir={"": "src"},
     packages=find_packages("src"),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='aaron',
-    maintainer_email='aaronray@mit.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="aaron",
+    maintainer_email="aaronray@mit.edu",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
--- rsm_dcist/src/rsm_dcist/example1_monitor.py
+++ rsm_dcist/src/rsm_dcist/example1_monitor.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
-from spark_config.config import Config, register_config
+
 from dataclasses import dataclass
-from std_msgs.msg import String
+
 from ros_system_monitor.node_info import NodeInfo, Status
+from spark_config.config import Config, register_config
+from std_msgs.msg import String
+
 
 class Example1NodeMonitor:
     def __init__(self, config: Example1NodeMonitorConfig):
-
         self.divider = config.divider
         self.mod = config.mod
         self.monitor = None
-
 
     @classmethod
     def load(cls, path):
@@ -24,24 +25,27 @@ class Example1NodeMonitor:
         )
         monitor.add_subscribers([subscriber])
 
-
-
     def callback(self, msg):
-
-        msg_idx = int(msg.data.split(' ')[3])
+        msg_idx = int(msg.data.split(" ")[3])
         if msg_idx // self.divider % self.mod:
             status = Status.NOMINAL
         else:
             status = Status.ERROR
         note = f"Current messaged idx {msg_idx}"
-        info = NodeInfo(nickname="example1", node_name="<external monitor>", status=status, notes=note)
+        info = NodeInfo(
+            nickname="example1",
+            node_name="<external monitor>",
+            status=status,
+            notes=note,
+        )
 
         self.monitor.update_node_info(info)
 
 
-@register_config("external_monitor", name="Example1NodeMonitor", constructor=Example1NodeMonitor)
+@register_config(
+    "external_monitor", name="Example1NodeMonitor", constructor=Example1NodeMonitor
+)
 @dataclass
 class Example1NodeMonitorConfig(Config):
     divider: int = None
     mod: int = None
-
