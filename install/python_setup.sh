a2f2c0ca0568c88d392a00471e68e6872438b0f2
Aaron Ray <aaronray@mit.edu>
2025-04-11 09:30:50 -0400

    Feature/adt4 env (#15)
    * Switch to ADT4 env var prefix
    
    * finish updating
    
    * remove extra launch file

--- install/python_setup.sh
+++ install/python_setup.sh
@@ -1,12 +1,12 @@
 source ~/.zshrc
-mkdir -p $DCIST_ENV
+mkdir -p $ADT4_ENV
 
 ##################################
 # Install ROMAN
 ##################################
-python3 -m virtualenv $DCIST_ENV/roman --download
-source $DCIST_ENV/roman/bin/activate
-pushd $DCIST_WS/src/awesome_dcist_t4/roman
+python3 -m venv $ADT4_ENV/roman
+source $ADT4_ENV/roman/bin/activate
+pushd $ADT4_WS/src/awesome_dcist_t4/roman
 
 # Build CLIPPER
 git submodule update --init --recursive
@@ -15,13 +15,13 @@ cd dependencies/clipper/build
 cmake .. && make && make pip-install
 
 # Pip installs
-cd $DCIST_WS/src/awesome_dcist_t4/roman
+cd $ADT4_WS/src/awesome_dcist_t4/roman
 pip install . --no-deps
-pip install -r $DCIST_WS/src/awesome_dcist_t4/install/roman_requirements.txt
+pip install -r $ADT4_WS/src/awesome_dcist_t4/install/roman_requirements.txt
 
 # Download FastSAM weights
-mkdir -p $DCIST_WS/weights
-cd $DCIST_WS/weights
+mkdir -p $ADT4_WS/weights
+cd $ADT4_WS/weights
 gdown 'https://drive.google.com/uc?id=1m1sjY4ihXBU1fZXdQ-Xdj-mDltW-2Rqv'
 
 popd
