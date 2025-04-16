4272cfcf3c1e943d8918027d18daacb0b1f360f0
Aaron Ray <aaronray@mit.edu>
2025-04-16 16:41:41 -0400

    Config ci (#17)
    * check configs in ci
    
    * wip
    
    * fixup
    
    * fixup
    
    * fixup
    
    * fixup
    
    * should fail ci
    
    * should pass ci
    
    * cleanup yaml install
    
    * fixup

---
+++ dcist_launch_system/scripts/check_configs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd $ADT4_WS/src/awesome_dcist_t4/dcist_launch_system
+if [[ `git status --porcelain` ]]; then
+    echo "Diff detected *before* config generation. Should not happen!"
+    exit 1
+fi
+
+source ${ADT4_ENV}/spark_env/bin/activate
+./scripts/generate_configs.sh
+
+if [[ `git status --porcelain` ]]; then
+    echo "Diff detected. Committed configs inconsistent with config generators"
+    exit 1
+fi
