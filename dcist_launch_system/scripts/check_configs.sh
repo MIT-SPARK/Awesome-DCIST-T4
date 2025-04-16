b68e11a039e868184c2268eca1460db338233068
Aaron Ray <aaronray@mit.edu>
2025-04-16 01:20:18 +0000

    check configs in ci

---
+++ dcist_launch_system/scripts/check_configs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd $ADT4_WS/src/awesome_dcist_t4/dcist_launch_system
+if [[ `git status --porcelain` ]]; then
+    echo "Diff detected *before* config generation. Should not happen!"
+    exit 1
+fi
+
+./scripts/generate_configs.sh
+
+if [[ `git status --porcelain` ]]; then
+    echo "Diff detected. Committed configs inconsistent with config generators"
+    exit 1
+fi
