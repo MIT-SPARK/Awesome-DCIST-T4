4860aea31af50482707e8a141c8e8af97f0f5ff6
Aaron Ray <aaronray@mit.edu>
2025-04-25 14:59:38 +0000

    should fail ci for the correct reason

--- dcist_launch_system/scripts/test_python_imports.py
+++ dcist_launch_system/scripts/test_python_imports.py
@@ -1,5 +1,5 @@
+from omniplanner_ros.omniplanner_node import *
 from ros_system_monitor.ros_system_monitor import *
 from spot_tools_ros.spot_executor_ros import *
-from omniplanner_ros.omniplanner_node import *
 
 from dcist_launch_system.prior_dsg_publisher_node import *
