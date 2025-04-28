1b3b1911fd1efad9df2afaaba1a787ca63dd3a1a
Aaron Ray <aaronray@mit.edu>
2025-04-28 19:05:06 -0400

    Fix config check not running in CI (#30)
    * should fail ci?
    
    * fix ci failing config check
    
    * fix config gen

--- dcist_launch_system/scripts/check_configs.sh
+++ dcist_launch_system/scripts/check_configs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 cd $ADT4_WS/src/awesome_dcist_t4/dcist_launch_system
 if [[ `git status --porcelain` ]]; then
     echo "Diff detected *before* config generation. Should not happen!"
