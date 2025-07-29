198ae7554c59a1baa9c9898c61b0abd7101b3f6c
robot computer <aerospacecontrolslabmit@gmail.com>
2025-07-29 14:40:51 -0400

    update topaz camera calibration and add spot recording scripts

---
+++ scripts/record_spot_calibration.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+ros2 bag record \
+    /tf \
+    /tf_static \
+    /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/rgb/image_rect_color/compressed \
+    /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/rgb/camera_info \
+    /${ADT4_ROBOT_NAME}/frontleft/color/image_raw/compressed \
+    /${ADT4_ROBOT_NAME}/frontleft/color/camera_info \
+    $@
