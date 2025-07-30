30e8465f0594d94f3f612d4be7ffe8fc05fd9ed6
Mason Peterson <mbpeterson70@gmail.com>
2025-07-30 14:02:11 -0400

    remove recording raw images in record script
    instead only record compressed

--- scripts/record_spot_sensors.bash
+++ scripts/record_spot_sensors.bash
@@ -5,7 +5,6 @@ ros2 bag record \
     /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/depth/camera_info \
     /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/depth/depth_registered \
     /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/rgb/image_rect_color/compressed \
-    /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/rgb/image_rect_color \
     /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/rgb/camera_info \
     /${ADT4_ROBOT_NAME}/joint_states \
     $@
