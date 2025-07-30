e3abd182afa2b40111d21071e16fb3440388172f
robot computer <aerospacecontrolslabmit@gmail.com>
2025-07-30 14:11:35 -0400

    record raw for now instead of compressed

--- scripts/record_spot_sensors.bash
+++ scripts/record_spot_sensors.bash
@@ -4,7 +4,7 @@ ros2 bag record \
     /tf_static \
     /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/depth/camera_info \
     /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/depth/depth_registered \
-    /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/rgb/image_rect_color/compressed \
+    /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/rgb/image_rect_color \
     /${ADT4_ROBOT_NAME}/${ADT4_ROBOT_NAME}_zed/rgb/camera_info \
     /${ADT4_ROBOT_NAME}/joint_states \
     $@
