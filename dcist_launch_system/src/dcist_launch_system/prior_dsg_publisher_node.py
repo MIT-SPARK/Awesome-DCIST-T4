be14b2493adccca808b0c89ac1a98066ae509d9b
Ruihan Xu (Multy) <rhxu@umich.edu>
2025-08-19 22:19:10 -0400

    Update prior_dsg_publisher_node.py

--- dcist_launch_system/src/dcist_launch_system/prior_dsg_publisher_node.py
+++ dcist_launch_system/src/dcist_launch_system/prior_dsg_publisher_node.py
@@ -50,8 +50,8 @@ class PriorDsgPublisher(Node):
         self.timer = self.create_timer(timer_period_s, self.publish_dsg)
 
     def publish_dsg(self):
-        self.dsg_sender.publish(self.G)
-        self.get_logger().info("Published map", frame_id="map")
+        self.dsg_sender.publish(self.G, frame_id="map")
+        self.get_logger().info("Published map")
 
 
 def main(args=None):
