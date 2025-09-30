97a368dbc050ac761c4972ddaab767af9c39322d
harelb <harelb@mit.edu>
2025-09-30 11:03:46 -0400

    updated clone script to make  sure missing permissions on submoudles don't break the build

--- docker/prepare_src.sh
+++ docker/prepare_src.sh
@@ -10,10 +10,21 @@ if [ -d "$SRC_DIR" ]; then
     rm -rf "$SRC_DIR"
 fi
 
-echo "--- Cloning Awesome-DCIST-T4 and its submodules into '$SRC_DIR'... ---"
-# Use the SSH URL for the main repository
-git clone git@github.com:MIT-SPARK/Awesome-DCIST-T4.git "$SRC_DIR/awesome_dcist_t4" --recursive
+# Step 1: Clone only the main repository first
+echo "--- Cloning Awesome-DCIST-T4 into '$SRC_DIR/awesome_dcist_t4'... ---"
+git clone git@github.com:MIT-SPARK/Awesome-DCIST-T4.git "$SRC_DIR/awesome_dcist_t4"
 
+# Use pushd to navigate into the repository directory
+pushd "$SRC_DIR/awesome_dcist_t4" > /dev/null
+
+# Step 2: Initialize and clone all submodules efficiently and resiliently
+echo "--- Cloning all submodules (shallow clone, will continue on failure)... ---"
+git submodule update --init --depth 1 --recursive || true
+
+# Use popd to return to the original directory
+popd > /dev/null
+
+# Step 3: Import additional packages as before
 echo "--- Importing additional packages with vcs... ---"
 vcs import "$SRC_DIR" < "$SRC_DIR/awesome_dcist_t4/install/packages.yaml"
 
