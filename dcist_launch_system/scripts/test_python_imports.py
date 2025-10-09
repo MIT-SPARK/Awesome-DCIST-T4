feed12c75bbea81f9b554e46ac4e5433ac07ad96
Aaron Ray <aaronray@mit.edu>
2025-10-09 17:13:40 -0400

    Feature/chatdsg (#218)
    * Add heracles and heracles_agents
    
    * Fix nlu_instruction_panel status monitoring
    
    * Added base_statin_heracles and spot_prior_dsg_heracles
    
    * Add chatdsg window (connection to scene graph still not working)
    
    * Chatdsg should work
    
    * Update README env vars; Update configs
    
    * Bump heracles (use ros params for neo4j user/pass)
    
    * Dependency updates
    
    * Add pydantic_settings python dep
    
    * Linting
    
    * Update requirements, remap heracles openai key
    
    * bump heracles
    
    * bump heracles_agents
    
    ---------
    
    Co-authored-by: Jake Arkin <jarkin@mit.edu>

--- dcist_launch_system/scripts/test_python_imports.py
+++ dcist_launch_system/scripts/test_python_imports.py
@@ -1,6 +1,9 @@
+from heracles_agents.llm_agent import *
+from heracles_agents.llm_interface import *
 from nlu_interface import *
 from omniplanner_ros.omniplanner_node import *
 from ros_system_monitor.ros_system_monitor import *
 from spot_tools_ros.spot_executor_ros import *
+from textual import *
 
 from dcist_launch_system.prior_dsg_publisher_node import *
